a0b84f3527b0a0914c9b4570f6e9f0ccb4bc6a9b
Mate <mategavran32@gmail.com>
2023-03-22 23:03:49 +0100

    changes 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/d143e40a-c62f-4271-9242-a6bcc7968422" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Submit the form belowe or shoot me an email - mategavran32@gmail.com</p>
+                <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - mategavran32@gmail.com</p>
             </div>
             <input className="p-2 bg-[#ccd6f6]" type="text" placeholder='Name' name='name' />
             <input className="my-4 p-2 bg-[#ccd6f6]" type="email" placeholder='Email' name='email' />
